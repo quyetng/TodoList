25a8fe846c6e00f46dbed0bcbe84451492df65cd
QuyetNguyen <quyetnguyentan@gmail.com>
2019-12-01 13:15:17 -0700

    Add data peristed functionality complete

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class ViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggs", "Clean House"]
-    
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,7 +59,7 @@ class ViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
-            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
